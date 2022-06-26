cde87ee7c7025259b727d38366634da6e4936331
Anna G <gumulka@student.agh.edu.pl>
2022-06-26 17:46:13 +0200

    lista partic

--- src/main/frontend/src/meetings/MeetingsPage.vue
+++ src/main/frontend/src/meetings/MeetingsPage.vue
@@ -35,12 +35,15 @@
                 this.meetings.push(meeting);
             },
             addMeetingParticipant(meeting) {
+			this.addParticipantToMeeting(meeting)
                 meeting.participants.push(this.username);
             },
             removeMeetingParticipant(meeting) {
+			this.deleteParticipantFromMeeting(this.username);
                 meeting.participants.splice(meeting.participants.indexOf(this.username), 1);
             },
             deleteMeeting(meeting) {
+			this.delMeeting(meeting);
                 this.meetings.splice(this.meetings.indexOf(meeting), 1);
             },
 			
@@ -48,10 +51,35 @@
 				var x = this.$http.get('meetings')
 				.then(response => {
                         this.meetings = response.body;
+						this.meetings.forEach(meeting=>this.loadParticipants(meeting));
                     });
 				console.log(x);
 				return x;
 			
+			},
+			
+			loadParticipants(meeting){
+			var x = this.$http.get('meetings/'+meeting.id+'/participants')
+			.then(response => {
+                        meeting.participants = response.body;
+						
+                    });
+			
+			},
+			
+			addParticipantToMeeting(meeting){
+			// "/{id}/add" PUT
+			this.$http.put('meetings/'+meeting.id+'/add');
+			},
+			
+			deleteParticipantFromMeeting(username){
+			// "/{id}/delete"
+			this.$http.delete('meetings/'+username.id+'/delete');
+			},
+			
+			delMeeting(meeting){
+			//"/{id}" DELETE
+			this.$http.delete('meetings/'+meeting.id);
 			}
         }
     }
